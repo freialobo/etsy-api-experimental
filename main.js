fb9e82bffcdc143f2e338f2881f2f423d773729d
Havaldar <abhinavhavaldar@hotmail.com>
2015-11-06 01:00:23 -0500

    done

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 $(document).ready(function() {
 	var keystring = "4mt41mv07mvz76ndjfmwm8lh";
 	var shared_secret = "w09lqqp3jz";
-	var url = "";
+	var url = "https://openapi.etsy.com/v2/listings/active?api_key=4mt41mv07mvz76ndjfmwm8lh&color_triplet=#";
 	$("input").click(function() {
-		
-		console.log($( "input:checked" ).val());
-		$.get("",function(data) {
-			console.log(data);
-		});
+		url += $("input:checked").val();
+		$.ajax({
+            type: "GET",
+            url: url,
+            async:true,
+            crossDomain:true,
+            success: function(data, status, xhr) {
+                var output = ""
+                for(var i in data.results){
+                	output += "<li>" + i.title + "</li>";
+                }
+                $(".items").html(output);
+            }
+        });
 	});
 });
