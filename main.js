5aaa1b3300d23ed6b6de3b36574fc5438f5a28c2
Havaldar <abhinavhavaldar@hotmail.com>
2015-11-06 00:27:51 -0500

    jquery stuff

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-main.js
+$(document).ready(function() {
+	var keystring = "4mt41mv07mvz76ndjfmwm8lh";
+	var shared_secret = "w09lqqp3jz";
+	var url = "";
+	$("input").click(function() {
+		
+		console.log($( "input:checked" ).val());
+		$.get("",function(data) {
+			console.log(data);
+		});
+	});
+});
